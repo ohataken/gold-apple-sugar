57eed4c9219d38c0e466a74daf681359bdeeef11
Ken Ohata <ohataken@outlook.com>
2022-07-06 18:07:08 +0900

    root page

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
+import fs from 'fs';
 import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
 
 const server: FastifyInstance = Fastify({});
 
 server.get('/ping', async (request, reply) => {
   return { pong: 'it worked!' };
+});
+
+server.get('/', (request, reply) => {
+  const stream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+  reply.send(stream);
 });
 
 server.listen({ port: parseInt(process.env.PORT || "3000"), host: "0.0.0.0" });
