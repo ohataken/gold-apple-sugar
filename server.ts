5222d2568b88af065da6ee85805f04c4cbcdb8a4
Ken Ohata <ohataken@outlook.com>
2022-07-06 19:07:27 +0900

    :construction: /oauth2/redirect

--- server.ts
+++ server.ts
@@ -1,7 +1,18 @@
 import fs from 'fs';
+import { createGoogleOAuth2Client, getGoogleOAuth2Scopes } from './lib/google_oauth2_client';
 import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
 
 const server: FastifyInstance = Fastify({});
+
+server.get('/oauth2/redirect', (request, reply) => {
+  const client = createGoogleOAuth2Client();
+  const url = client.generateAuthUrl({
+    access_type: "offline",
+    scope: getGoogleOAuth2Scopes(),
+  });
+
+  reply.redirect(302, url);
+});
 
 server.get('/ping', async (request, reply) => {
   return { pong: 'it worked!' };
